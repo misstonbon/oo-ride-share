17016ce69f2089abd1d77e4992cdaa6374b711a5
Tanja Stroble <plusdeltah@gmail.com>
2017-10-06 11:08:37 -0700

    updated pseudocode for pax

---
+++ lib/passenger.rb
@@ -0,0 +1,30 @@
+class Passenger
+
+should have :
+
+
+id
+name 
+phone number
+
+should do :
+
+  retrieve list of @trip instances individual passenger has taken
+
+  retrieve list of all previosu driver instances through trip functionality set up in driver ???????
+  this is not clear at all.
+  ??????????
+
+
+  retrive passengers from CSV
+
+  find passenger by id
+
+
+
+
+
+
+
+
+end
