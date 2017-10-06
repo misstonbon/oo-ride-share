1a2d89512741ca24270e52d80fe1f12cea020fed
Tanja Stroble <plusdeltah@gmail.com>
2017-10-06 11:05:13 -0700

    pseudocode for trips added

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,5 +1,27 @@
 class Trip
 
+should have:
+
+id
+passenger id
+driver id
+date
+rating  1-5 only
+
+should do :
+
+  retrieve associated driver instan e thru passenger id
+
+  retrieve @passenger through its id
+
+  find all trips  for pax id
+
+  retrieve all trips from CSV
+
+
+  
+
+
 
 
 
