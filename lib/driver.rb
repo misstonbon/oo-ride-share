ed998d1c162fa3e3cdb2a45e2d8a8b0f7fbf1073
Tanja Stroble <plusdeltah@gmail.com>
2017-10-06 10:29:10 -0700

    pseudo code added

---
+++ lib/driver.rb
@@ -0,0 +1,23 @@
+class Driver
+
+
+  should have:
+
+
+  ID
+  name
+  VIN regex for 17 characters and the right format ?
+
+  should do :
+
+  retrieve the list of @trip instances each driver has
+
+  retrieve average rating for all trips for driver
+
+  retrieve all drivers from CSV
+
+  find driver by id
+
+
+
+end
---
+++ lib/trip.rb
@@ -0,0 +1,8 @@
+class Trip
+
+
+
+
+
+
+end
